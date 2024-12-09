e513e79c64020a67157f6502e782f18fd9cdf82e
Gabriel Alves <gabhenriquesa1@gmail.com>
2024-12-09 17:27:37 -0300

    feat: new bd

--- back/bd/bdTemporary.sql
+++ back/bd/bdTemporary.sql
@@ -1,117 +1,88 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 09/12/2024 às 02:35
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `bdusers`
---
-CREATE DATABASE IF NOT EXISTS `bdusers` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `bdusers`;
-
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.4.32-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.8.0.6908
 -- --------------------------------------------------------
 
---
--- Estrutura para tabela `calendario`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE `calendario` (
-  `id` int(11) NOT NULL,
+
+-- Dumping database structure for bdUsers
+CREATE DATABASE IF NOT EXISTS `bdusers` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `bdUsers`;
+
+-- Dumping structure for table bdUsers.calendario
+CREATE TABLE IF NOT EXISTS `calendario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(50) DEFAULT NULL,
   `date` varchar(50) DEFAULT NULL,
   `color` varchar(50) DEFAULT NULL,
   `description` longtext DEFAULT NULL,
-  `curso` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `curso` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Despejando dados para a tabela `calendario`
---
-
+-- Dumping data for table bdUsers.calendario: ~2 rows (approximately)
 INSERT INTO `calendario` (`id`, `title`, `date`, `color`, `description`, `curso`) VALUES
-(28, 'Natal', '2024-12-25', '#f72222', 'Meery Chritsmas', 'Ciência da Computação'),
-(29, 'Substitutiva de Grafos', '2024-12-26', '#0c6a92', 'Dia do abalo', 'Ciência da Computação'),
-(30, 'Entrega de Web', '2024-12-18', '#a27676', 'Entregar o Trabalho de Programação Web', 'Ciência da Computação'),
-(31, 'Dia D', '2024-12-12', '#000000', 'O \"D\" do \"dia D\" é de \"Drama\", ou seja, \"Dia do Drama\"', 'Ciência da Computação');
+	(32, 'Ano Novo', '2024-12-31', '#005799', 'Virada do Ano', 'Sistema da Informação'),
+	(56, 'Entrega de Web', '2024-12-11', '#000000', 'Entregar o Trabalho de Programação Web', 'Ciência da Computação');
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `cursos`
---
-
-CREATE TABLE `cursos` (
-  `id` int(11) NOT NULL,
+-- Dumping structure for table bdUsers.cursos
+CREATE TABLE IF NOT EXISTS `cursos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `sigla` varchar(255) NOT NULL,
   `codigo` varchar(255) NOT NULL,
   `curso` varchar(50) DEFAULT NULL,
-  `coordenador` varchar(50) DEFAULT 'Farinhas'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `coordenador` varchar(50) DEFAULT 'Farinhas',
+  PRIMARY KEY (`id`),
+  KEY `curso` (`curso`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Despejando dados para a tabela `cursos`
---
-
+-- Dumping data for table bdUsers.cursos: ~3 rows (approximately)
 INSERT INTO `cursos` (`id`, `sigla`, `codigo`, `curso`, `coordenador`) VALUES
-(1, 'CCO', '001', 'Ciência da Computação', 'Rafael Frinhani'),
-(2, 'SIN', '002', 'Sistema da Informação', 'Phyllipe de Souza Lima'),
-(3, 'MAT', '003', 'Matemática', 'Fernando Pereira Micena');
+	(1, 'CCO', '001', 'Ciência da Computação', 'Rafael Frinhani'),
+	(2, 'SIN', '002', 'Sistema da Informação', 'Phyllipe de Souza Lima'),
+	(3, 'MAT', '003', 'Matemática', 'Fernando Pereira Micena');
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `turmas`
---
-
-CREATE TABLE `turmas` (
-  `id` int(11) NOT NULL,
+-- Dumping structure for table bdUsers.turmas
+CREATE TABLE IF NOT EXISTS `turmas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) NOT NULL,
   `sigla` varchar(255) NOT NULL,
   `participantes` varchar(50) DEFAULT NULL,
   `professor` varchar(50) DEFAULT NULL,
-  `Vagas` int(11) DEFAULT NULL,
+  `vagas` int(11) DEFAULT NULL,
   `ano` int(11) DEFAULT NULL,
   `horarios` longtext DEFAULT NULL,
   `periodo` int(11) DEFAULT NULL,
   `cargaHoraria` int(11) DEFAULT NULL,
   `curso` varchar(50) DEFAULT NULL,
-  `solicitacoes` longtext DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `solicitacoes` longtext DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Despejando dados para a tabela `turmas`
---
+-- Dumping data for table bdUsers.turmas: ~8 rows (approximately)
+INSERT INTO `turmas` (`id`, `nome`, `sigla`, `participantes`, `professor`, `vagas`, `ano`, `horarios`, `periodo`, `cargaHoraria`, `curso`, `solicitacoes`) VALUES
+	(40, 'Inteligência Artificial', 'CMC01', '', 'Isabella Drumond', 60, 2024, 'Terça-Feira  13:30 - 15:20,Quinta-Feira  13:30 - 15:20,', 5, 64, 'Ciência da Computação', '2004'),
+	(41, 'Cálculo A', 'MAT00A', '2004', 'Juan Valentin', 110, 2024, 'Segunda-Feira  7:55 - 12:00,', 1, 64, 'Matemática Bacharelado', '2004'),
+	(42, 'Sistemas Operacionais', 'SRC02', '2004', 'Carlos Minoru', 60, 2024, 'Quarta-Feira  13:30 - 15:20,Quinta-Feira  15:45 - 17:35,', 3, 64, 'Sistema da Informação', ''),
+	(43, 'Banco de Dados I', 'SPAD01', '', 'Melise Vieira', 70, 2024, 'Segunda-Feira  15:45 - 17:35,Quarta-Feira  7:55 - 9:45,', 5, 64, 'Sistema da Informação', ''),
+	(44, 'Desenvolvimento de Jogos', 'SDES01', '2004', 'Phyllipe Lima', 30, 2024, 'Quinta-Feira  19:00 - 20:40,Terça-Feira  21:00 - 22:40,', 0, 64, 'Sistema da Informação', ''),
+	(45, 'Economia da Informação', 'SECO01', '', 'José Arnaldo', 40, 2024, 'Quinta-Feira  13:30 - 15:20,', 0, 32, 'Sistema da Informação', ''),
+	(46, 'Sistemas Embarcados', 'CRSC02', '2004', 'Luiz Olmes', 1, 2024, 'Quarta-Feira  7:55 - 12:00,', 3, 64, 'Sistema da Informação', ''),
+	(47, 'Programação Lógica Funcional', 'PRSC03', '2004', 'Isabela Neves', 60, 2024, 'Segunda-Feira  13:30 - 15:20,Sexta-Feira  13:30 - 15:20,', 2, 64, 'Sistema da Informação', '');
 
-INSERT INTO `turmas` (`id`, `nome`, `sigla`, `participantes`, `professor`, `Vagas`, `ano`, `horarios`, `periodo`, `cargaHoraria`, `curso`, `solicitacoes`) VALUES
-(40, 'Inteligência Artificial', 'CMC01', '', 'Isabella Drumond', 60, 2024, 'Terça-Feira  13:30 - 15:20,Quinta-Feira  13:30 - 15:20,', 5, 64, 'Ciência da Computação', '2002,2000'),
-(41, 'Cálculo A', 'MAT00A', '', 'Juan Valentin', 110, 2024, 'Segunda-Feira  7:55 - 12:00,', 1, 64, 'Matemática Bacharelado', '2002,2004'),
-(42, 'Sistemas Operacionais', 'SRC02', '', 'Carlos Minoru', 60, 2024, 'Quarta-Feira  13:30 - 15:20,Quinta-Feira  15:45 - 17:35,', 3, 64, 'Sistema da Informação', '2002,2000,2004'),
-(43, 'Banco de Dados I', 'SPAD01', '', 'Melise Vieira', 60, 2024, 'Segunda-Feira  15:45 - 17:35,Quarta-Feira  7:55 - 9:45,', 5, 64, 'Sistema da Informação', '2002,2001,2000,2005,2004'),
-(44, 'Desenvolvimento de Jogos', 'SDES01', '', 'Phyllipe Lima', 30, 2024, 'Quinta-Feira  19:00 - 20:40,Terça-Feira  21:00 - 22:40,', 0, 64, 'Sistema da Informação', '2002,2000,2004'),
-(45, 'Economia da Informação', 'SECO01', '', 'José Arnaldo', 40, 2024, 'Quinta-Feira  13:30 - 15:20,', 0, 32, 'Sistema da Informação', '2002,2004');
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+-- Dumping structure for table bdUsers.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `senha` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
@@ -121,85 +92,28 @@ CREATE TABLE `users` (
   `curso` varchar(50) NOT NULL DEFAULT 'Sistema da Informação',
   `solicitacoes` longtext DEFAULT NULL,
   `reset_token` varchar(128) DEFAULT NULL,
-  `reset_token_expires` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `reset_token_expires` timestamp NULL DEFAULT NULL,
+  `turmas` longtext DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `matricula` (`matricula`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Despejando dados para a tabela `users`
---
+-- Dumping data for table bdUsers.users: ~11 rows (approximately)
+INSERT INTO `users` (`id`, `name`, `senha`, `email`, `status`, `matricula`, `type`, `curso`, `solicitacoes`, `reset_token`, `reset_token_expires`, `turmas`) VALUES
+	(2, 'Marcio Morelo', 'Senha123@', 'marcioMorelo@gmail.com', 1, '2002', 0, 'Sistema da Informação', '', NULL, NULL, NULL),
+	(3, 'Joao Aparecido', 'Senha123@', 'soulmaste58@gmail.com', 1, '2003', 0, 'Sistema da Informação', '', NULL, NULL, NULL),
+	(4, 'Caio Mendes Ribeiro da Rosa', 'Caio123@', 'caioribeirorosa@hotmail.com', 1, '2004', 0, 'Sistema da Informação', '40,,41', NULL, NULL, '46,41,47,42,44'),
+	(5, 'Phyllipe de Souza Lima', 'Senha123@', 'coordsin@gmail.com', 1, '2009001', 1, 'Sistema da Informação', '', NULL, NULL, NULL),
+	(6, 'Rafael Frinhani', 'Senha123@', 'coordcco@gmail.com', 1, '1998001', 1, 'Ciência da Computação', '', NULL, NULL, NULL),
+	(10, 'Fernando Pereira Micena', 'Senha123@', 'coordmat@gmail.com', 1, '2009002', 1, 'Matemática Bacharelado', '', NULL, NULL, NULL),
+	(11, 'Davi Dias', 'Senha123@', 'davi@gmail.com', 1, '2001', 0, 'Ciência da Computação', '', NULL, NULL, NULL),
+	(12, 'Gabriel Alves', 'Senha123@', ' gabriel@gmail.com', 1, '2000', 0, 'Sistema da Informação', '', NULL, NULL, NULL),
+	(13, 'Tiago Reis', 'Senha123@', 'tiago@gmail.com', 1, '2005', 0, 'Sistema da Informação', '', NULL, NULL, NULL),
+	(14, 'Paulo Tejano', 'Senha123@', 'pauloteste@gmail.com', 1, '1999', 0, 'Sistema da Informação', '', NULL, NULL, NULL);
 
-INSERT INTO `users` (`id`, `name`, `senha`, `email`, `status`, `matricula`, `type`, `curso`, `solicitacoes`, `reset_token`, `reset_token_expires`) VALUES
-(2, 'Marcio Morelo', 'Senha123@', 'marcioMorelo@gmail.com', 1, '2002', 0, 'Sistema da Informação', '43', NULL, NULL),
-(3, 'Joao Aparecido', 'Senha123@', 'soulmaste58@gmail.com', 1, '2003', 0, 'Sistema da Informação', '43', NULL, NULL),
-(4, 'Caio Mendes Ribeiro da Rosa', 'Caio123@', 'caioribeirorosa@hotmail.com', 1, '2004', 0, 'Sistema da Informação', '42,44,45,43,,41', NULL, NULL),
-(5, 'Phyllipe de Souza Lima', 'Senha123@', 'coordsin@gmail.com', 1, '2009001', 1, 'Sistema da Informação', '', NULL, NULL),
-(6, 'Rafael Frinhani', 'Senha123@', 'coordcco@gmail.com', 1, '1998001', 1, 'Ciência da Computação', '', NULL, NULL),
-(10, 'Fernando Pereira Micena', 'Senha123@', 'coordmat@gmail.com', 1, '2009002', 1, 'Matemática Bacharelado', '', NULL, NULL),
-(11, 'Davi Dias', 'Senha123@', 'davi@gmail.com', 1, '2001', 0, 'Ciência da Computação', '43', NULL, NULL),
-(12, 'Gabriel Alves', 'Senha123@', ' gabriel@gmail.com', 1, '2000', 0, 'Sistema da Informação', '43,42,44,40', NULL, NULL),
-(13, 'Tiago Reis', 'Senha123@', 'tiago@gmail.com', 1, '2005', 0, 'Sistema da Informação', '43', NULL, NULL),
-(14, 'Paulo', 'Senha123@', 'pauloteste@gmail.com', 1, '1999', 0, 'Sistema da Informação', 'Matemática', NULL, NULL);
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `calendario`
---
-ALTER TABLE `calendario`
-  ADD PRIMARY KEY (`id`) USING BTREE;
-
---
--- Índices de tabela `cursos`
---
-ALTER TABLE `cursos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `curso` (`curso`);
-
---
--- Índices de tabela `turmas`
---
-ALTER TABLE `turmas`
-  ADD PRIMARY KEY (`id`) USING BTREE;
-
---
--- Índices de tabela `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `matricula` (`matricula`);
-
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `calendario`
---
-ALTER TABLE `calendario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
-
---
--- AUTO_INCREMENT de tabela `cursos`
---
-ALTER TABLE `cursos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de tabela `turmas`
---
-ALTER TABLE `turmas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
-
---
--- AUTO_INCREMENT de tabela `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-COMMIT;
-
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
